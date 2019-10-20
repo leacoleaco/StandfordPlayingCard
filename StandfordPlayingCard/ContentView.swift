84c4dcca68fe5fb11d79553f622934a1c2d130a2
Leaco <leaco9012@163.com>
2019-10-20 22:54:42 +0800

    Initial Commit

---
+++ StandfordPlayingCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StandfordPlayingCard
+//
+//  Created by leaco on 2019/10/20.
+//  Copyright © 2019 Leaco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
