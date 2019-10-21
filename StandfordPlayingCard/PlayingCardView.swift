c08b97f142790c5f30b53ff4e472493c9b44317a
leaco <leaco9012@163.com>
2019-10-21 13:32:34 +0800

    add view

---
+++ StandfordPlayingCard/PlayingCardView.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingCardView.swift
+//  StandfordPlayingCard
+//
+//  Created by 李科 on 2019/10/21.
+//  Copyright © 2019 Leaco. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+       
+    }
+
+}
