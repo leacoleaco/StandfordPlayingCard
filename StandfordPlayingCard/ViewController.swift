908b7be2952225f1bfbdc0a297e1de7abbba2d06
Leaco <leaco9012@163.com>
2019-10-24 00:00:47 +0800

    增加动画效果

--- StandfordPlayingCard/ViewController.swift
+++ StandfordPlayingCard/ViewController.swift
@@ -12,43 +12,41 @@ class ViewController: UIViewController {
 
     var deck = PlayingCardDeck()
 
-    @IBOutlet weak var playingCardView: PlayingCardView!{
-        didSet {
-            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipe.direction = [.left,.right]
-            playingCardView.addGestureRecognizer(swipe)
-            
-            let pinch = UIPinchGestureRecognizer(target: playingCardView , action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
-            playingCardView.addGestureRecognizer(pinch)
+    @IBOutlet var cardViews: [PlayingCardView]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var cards = [PlayingCard]()
+        for _ in 1...((cardViews.count+1)/2){
+            let card = deck.draw()!
+            cards += [card,card]
         }
+        for cardView in cardViews {
+            cardView.isFaceUp = false
+            let card = cards.remove(at: cards.count.arc4random)
+            cardView.rank=card.rank.order
+            cardView.suit=card.suit.rawValue
+            
+            cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
+        }
+
     }
     
-    @IBAction func filpCard(_ sender: UITapGestureRecognizer) {
-        switch sender.state {
+    @objc func flipCard(_ recognizer : UITapGestureRecognizer){
+        switch recognizer.state {
         case .ended:
-            playingCardView.isFaceUp = !playingCardView.isFaceUp
+            if let chooseCardView = recognizer.view as? PlayingCardView{
+                UIView.transition(with: chooseCardView,
+                                  duration:  0.6,
+                                  options: [.transitionFlipFromLeft],
+                                  animations: {
+                                    chooseCardView.isFaceUp = !chooseCardView.isFaceUp
+                                  })
+            }
         default:
             break
         }
-    }
-    
-    @objc func nextCard(){
-        if let card=deck.draw(){
-            playingCardView.rank=card.rank.order
-            playingCardView.suit=card.suit.rawValue
-        }
-         
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        for _ in 1...10 {
-            if let card = deck.draw() {
-                print("\(card)")
-            }
-
-        }
-
     }
 
 }
